0d713661fec8a481a6fc51f3fb7a8ef54ce604c0
leviathan88 <levigithuber@gmail.com>
2016-12-03 17:57:48 +0100

    Adding scss variables

--- client/components/flexbox/Flexbox.container.js
+++ client/components/flexbox/Flexbox.container.js
@@ -5,7 +5,7 @@ export default ()=> {
     <div id="flex-container">
       <div className="flex-child child1">1</div>
       <div className="flex-child child2">2</div>
-      <div style={{backgroundColor: '#ff8080'}} className="flex-child child3">3</div>      
+      <div className="flex-child child3">3</div>      
     </div>
   )
 }
