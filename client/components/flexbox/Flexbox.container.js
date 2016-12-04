44fa9767e6b6e06695179cae4d76fa8b2341a1a6
leviathan88 <levigithuber@gmail.com>
2016-12-04 19:14:25 +0100

    adding few more divs

--- client/components/flexbox/Flexbox.container.js
+++ client/components/flexbox/Flexbox.container.js
@@ -5,7 +5,10 @@ export default ()=> {
     <div id="flex-container">
       <div className="flex-child child1">1</div>
       <div className="flex-child child2">2</div>
-      <div className="flex-child child3">3</div>      
+      <div className="flex-child child3">3</div>
+      <div className="flex-child child4">4</div>
+      <div className="flex-child child5">5</div>
+      <div className="flex-child child6">6</div>
     </div>
   )
 }
