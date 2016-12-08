2ab828a975e470a64d5c9204bb6222f2e1dc97aa
leviathan88 <levigithuber@gmail.com>
2016-12-08 15:46:43 +0100

    learning flex-grow, shrink, basis

--- client/components/flexbox/Flexbox.container.js
+++ client/components/flexbox/Flexbox.container.js
@@ -2,13 +2,22 @@ import React from 'react'
 
 export default ()=> {
   return (
-    <div id="flex-container">
-      <div className="flex-child child1">1</div>
-      <div className="flex-child child2">2</div>
-      <div className="flex-child child3">3</div>
-      <div className="flex-child child4">4</div>
-      <div className="flex-child child5">5</div>
-      <div className="flex-child child6">6</div>
+    <div>
+      <div id="flex-container">
+        <div className="flex-child child1">U</div>
+        <div className="flex-child child2">M</div>
+        <div className="flex-child child3">A</div>
+        <div className="flex-child child4">R</div>
+        <div className="flex-child child5">5</div>
+        <div className="flex-child child6">6</div>
+      </div>
+
+      <div id="flex-basis-container">
+        <div className="fb-child fb-child1">H</div>
+        <div className="fb-child fb-child2">U</div>
+        <div className="fb-child fb-child3">T</div>
+      </div>
     </div>
+
   )
 }
