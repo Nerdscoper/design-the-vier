4e9727d5b30f0b168163368bb8febde744c4e917
hutko <sabahudindizdarevic@hotmail.com>
2016-12-09 17:54:39 +0100

    hutko new age

--- client/components/hutko/hutko.js
+++ client/components/hutko/hutko.js
@@ -3,42 +3,71 @@ import React, { Component } from 'react'
 export default class Hutko extends Component {
   render() {
     return(
-      <div id="jedan">
 
+<div>
+
+<div id="pozadina">
+
+
+
+
+
+<div>
+
+<center><h1 id="www">WWW.CAŠARO-IVOKUC.COM</h1></center>
 
 
 <center>
-<button><a href="http://localhost:8080/hutko/jedan">Hutko Jedan</a></button>
-<p><button><a href="http://localhost:8080/hutko/dva">Hutko Dva</a></button></p>
-
-
-
+<p id="naslov">Pomoc pri izradi Web stranice</p>
+<br/>
 </center>
+</div>
+<div id="slika">
+<img src="https://tuanthong138.files.wordpress.com/2016/10/html_css.jpg" alt="Me and my Dzedaj" height="200">
+</img><br/>
+</div>
 
-<center><h1><img src="https://scontent-frt3-1.xx.fbcdn.net/t31.0-8/14918890_203327496772929_1767442467284160816_o.jpg" alt="Me and my Dzedaj" height="200"></img><p>Pomoc pri izradi Web stranice</p></h1></center>
-<center><p>HTML is the standard markup language for creating Web pages.</p></center>
+<div id="text">
+<left>
+<p>HTML is the standard markup language for creating Web pages.<br/>
+HTML stands for Hyper Text Markup Language<br/>
+HTML describes the structure of Web pages using marku<br/>
+HTML elements are the building blocks of HTML pages<br/>
+HTML elements are represented by tags<br/>
+HTML tags label pieces of content such as "heading", "paragraph", "table", and so on
+Browsers do not display the HTML tags, but use them to render the content of the page<br/>
+</p>
+</left>
+</div>
+
+
 <center>
-<h1 id="meho">HTML stands for Hyper Text Markup Language</h1>
-
-<h2>HTML describes the structure of Web pages using marku</h2>
-<h3>HTML elements are the building blocks of HTML pages</h3>
-<h4>HTML elements are represented by tags</h4>
-<h5>HTML tags label pieces of content such as "heading", "paragraph", "table", and so on</h5>
-<h6>Browsers do not display the HTML tags, but use them to render the content of the page</h6>
+ <button>
+<a href="http://www.amusingtime.com/images/047/peace-bro-funny-goat-image.jpg">Klikni i Budi Pozdravljen</a>
+ </button>
 </center>
-
-
-<center><button><a href="http://www.amusingtime.com/images/047/peace-bro-funny-goat-image.jpg">Klikni i Budi Pozdravljen</a></button></center>
-<ul><b>
+<ul>
+ <b>
   <a href="https://www.facebook.com/profile.php?id=100012870392538"><li>HUTKO-klikni</li></a>
 <a href="https://www.facebook.com/husnija.becirin?fref=ts">  <li>LEVI-klikni</li></a>
 <a href="https://www.facebook.com/igramzandara?fref=ts">  <li>BENJO-klikni</li></a>
-</b></ul>
-<center><iframe width="854" height="480" src="https://www.youtube.com/embed/X7xOB694V2U" frameborder="0" allowfullscreen></iframe></center>
-<b/><center><iframe src="//giphy.com/embed/daUOBsa1OztxC" width="200" height="100" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="http://giphy.com/gifs/car-bad-tesla-daUOBsa1OztxC">GIF</a></p></center>
-
+ </b>
+</ul>
+<div id="youtube">
+<center>
+<iframe width="854" height="480" src="https://www.youtube.com/embed/X7xOB694V2U" frameborder="0" allowfullscreen></iframe>
+</center>
+</div>
+<b/>
+<div id="copyrights">
+<center>
+ <img src="https://copyright.columbia.edu/content/dam/templates/copyright/banner_copyright-advisory.png" height="100" width="200"></img>
+</center>
+</div>
 
 </div>
+</div>
+
     )
   }
 }
